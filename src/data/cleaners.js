6efc0cdb6c09ede3ab9f10e238011d8709e60309
Tarek <tetotarek711@gmail.com>
2026-05-20 14:12:25 +0300

    update

---
+++ src/data/cleaners.js
@@ -0,0 +1,15 @@
+import elnamlImg from "../assets/elnaml.png";
+
+const products = [
+
+    {
+        id: 1,
+        name: "منظف ارضيات 600 م",
+        price: 100,
+        category: "المنظفات",
+        image: elnamlImg,
+      },
+
+      ];
+
+export default products;
---
+++ src/data/pesticides.js
@@ -0,0 +1,43 @@
+import elnamlImg from "../assets/elnaml.png";
+import elpakImg from "../assets/elpak.png";
+import sraserImg from "../assets/sraser.png";
+import elnamosImg from "../assets/elnamos.png";
+
+const products = [
+
+  {
+    id: 1,
+    name: "مبيد النمل",
+    price: 100,
+    category: "المبيدات",
+    image: elnamlImg,
+  },
+
+  {
+    id: 2,
+    name: "مبيد البق",
+    price: 120,
+    category: "المبيدات",
+    image: elpakImg,
+  },
+
+  {
+    id: 3,
+    name: "مبيد الصراصير",
+    price: 150,
+    category: "المبيدات",
+    image: sraserImg,
+  },
+
+  {
+    id: 4,
+    name: "مبيد الناموس",
+    price: 130,
+    category: "المبيدات",
+    image: elnamosImg,
+  },
+
+ 
+];
+
+export default products;
