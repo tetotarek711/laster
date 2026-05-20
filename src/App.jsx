ef48439b3b23bcddd608a2ce9f2ab7bab4f7e08f
Tarek <tetotarek711@gmail.com>
2026-05-20 13:56:14 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import pesticidesImg from "./assets/Pesticides.png";
 import cleanersImg from "./assets/Detergents.png";
 import perfumesImg from "./assets/perfumes.png";
 import skinImg from "./assets/Beauty.png";
-import elnamlImg from "./assets/elnaml.png";
-import elpakImg from "./assets/elpak.png";
-import sraserImg from "./assets/sraser.png";
-import elnamosImg from "./assets/elnamos.png";
+import products from "./data/products";
 export default function App() {
 
   const categories = [
@@ -28,48 +25,6 @@ export default function App() {
   },
 ];
 
-const products = [
-
-  {
-    id: 1,
-    name: "مبيد النمل",
-    price: 100,
-    category: "المبيدات",
-    image: elnamlImg,
-  },
-
-  {
-    id: 2,
-    name: "مبيد البق",
-    price: 120,
-    category: "المبيدات",
-    image: elpakImg,
-  },
-
-  {
-    id: 3,
-    name: "مبيد الصراصير",
-    price: 150,
-    category: "المبيدات",
-    image: sraserImg,
-  },
-
-  {
-    id: 4,
-    name: "مبيد الناموس",
-    price: 130,
-    category: "المبيدات",
-    image: elnamosImg,
-  },
-
-  {
-    id: 5,
-    name: "منظف أرضيات",
-    price: 80,
-    category: "المنظفات",
-  },
-
-];
 const deliveryPrices = {
   "القاهرة الجديدة": 120,
   "القاهرة": 105,
